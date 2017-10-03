35228591f795b34cd62df35754deba390a21ea08
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 18:27:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments)
 {
-  var newArray=[]
+  var array=[]
   for(var i =0; i< musicians.length; i++)
   {
     //array.push(musicians[i] +' plays '+instruments[i]);
