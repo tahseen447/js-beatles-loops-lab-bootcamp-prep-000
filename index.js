2a828a0ca36bcdea47c1d005940d34f0e30bcdb9
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 18:20:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function theBeatlesPlay(musicians, instruments)
 {
   var newArray=[]
+  for(var i =0; i< musicians.length; i++)
+  {
+    array.push(musicians[i] +' plays '+instruments[i]);
+  }
   return array
 }
 function johnLennonFacts()
