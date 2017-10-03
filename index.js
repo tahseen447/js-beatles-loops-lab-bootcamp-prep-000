53e170ec68837e403fe1d103177188dac5034d43
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 18:13:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 function theBeatlesPlay(musicians, instruments)
+{}
+function johnLennonFacts()
+{}
+
+function iLoveTheBeatles(number)
+{
+  var array=[];
+  for(var i=0; i<=number; i++)
+  {
+    array.push('I Love the Beatles!')
+  }
+  return array
+}
