20225ef1ae8e626dd89dc9374a340b0fab2b46da
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 18:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ function iLoveTheBeatles(number)
   var array=[];
   if(number ===7)
   {
-    for(var i=0; i<=number; i++)
+    for(var i=0; i<number; i++)
   {
     array.push('I Love the Beatles!')
   }
+array.push('I Love the Beatles!')
 }
-else if(n===17)
+else if(number===17)
 {
   array.push('I Love the Beatles!')
 }
