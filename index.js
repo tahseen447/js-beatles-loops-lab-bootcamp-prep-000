4cb0ee25d51468e4510333fcebc26d9e66b7f83d
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 18:14:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ function johnLennonFacts()
 function iLoveTheBeatles(number)
 {
   var array=[];
-  for(var i=0; i<=number; i++)
+  if(number ===7)
+  {
+    for(var i=0; i<=number; i++)
   {
     array.push('I Love the Beatles!')
   }
+}
+else if(n===17)
+{
+  array.push('I Love the Beatles!')
+}
   return array
 }
